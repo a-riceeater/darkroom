1002bebb57c1a054537143b05eb06ee75a4d9b53
a-riceeater <elijb12@gmail.com>
2024-11-26 18:19:58 -0800

    base functions

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron')
+const sharp = require('sharp');
+const fs = require("fs");
+const { exec } = require('child_process');
+const im = require('imagemagick');
+/*
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('app/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})*/
+
+function generatePPM(input, output) {
+    exec(`dcraw -w -o 1 -q 3 ${input}`, async (err, stdout, stderr) => {
+        // this command takes the longest- should be used for each photo once when imported, then just keep the PPM for editing previews
+        if (err) {
+            console.error(`Error converting RAW file: ${stderr}`);
+            return;
+        }
+    })
+}
+
+function generateJPG(input, output) {
+    exec(`magick ${input} ${output}`, (err, stdout, stderr) => {
+        // this one is shorter, can be used for updating previews
+        if (err) {
+            console.error(`Error converting to JPEG: ${stderr}`);
+            return;
+        }
+    })
+}
